a0fddcd7172766e1e71ff85b3d38302af55b4cbe
AntoinAshraf <tonyashraf317@gmail.com>
2020-04-24 14:50:49 +0200

    addModelRepos

---
+++ ECommerceWebAPI/Repos/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceWebAPI.Repos
+{
+    public interface IOrderRepository {
+        IEnumerable<Order> GetOrders();
+        IEnumerable<Order> GetUserOrders(Guid UsrID);
+        void CreateOrder(Order order);
+        void UpdateOrder(Order product);
+        void DeleteOrder(Order order);
+    }
+}
---
+++ ECommerceWebAPI/Repos/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceWebAPI.Repos
+{
+    public interface IProductRepository {
+        IEnumerable<Product> GetProducts(Guid CategoryId);
+        IEnumerable<Product> GetProducts();
+        IEnumerable<Product> GetDiscountedProducts(Guid CategoryId);
+        Product GetProductByName(string ProductName); // Search by Name
+        void CreateProduct(Guid CategoryId, Product product);
+        void UpdateProducts(Product product);
+        void DeleteProduct(Product product);
+    }
+}
